49adc0f3a691744f195318c29d1cec865dfb1b30
Zhmy <1076996806@qq.com>
2024-12-10 16:12:07 +0800

    rose diagram

---
+++ RoseDiagram/wp_swhRoseDraw.m
@@ -0,0 +1,65 @@
+function [] = wp_swhRoseDraw(SwanFileFolderPath)
+%%  读取SWAN计算数据
+% BSG 9358 DSN 2027
+addpath('../CommonMethodology/');
+
+%"E:\张明阳\项目\SWAN_Taiwan_result\te_ST6_S6_2022.mat"
+files = dir(SwanFileFolderPath);
+filePaths = {};
+
+m1D = [];
+m2D = [];
+m3D = [];
+m4D = [];
+m1S = [];
+m2S = [];
+m3S = [];
+m4S = [];
+SWHall = [];
+DIRall = [];
+
+for i = 1:length(files)
+    % 检查是否是文件（排除 '.' 和 '..' 目录）
+    if ~files(i).isdir
+        % 构建完整的文件路径
+        fullPath = fullfile(SwanFileFolderPath, files(i).name);
+        % 存储到 cell 数组
+        filePaths{end+1} = fullPath; % 或者使用 filePaths = [filePaths; {fullPath}];
+    end
+end
+
+for year = 1:length(filePaths)
+    % BSG 9358 DSN 2027
+    SWH = wp_readMat(filePaths{year},2027,1);
+    DIR = wp_readMat(filePaths{year},2027,4);
+
+    SWHall = [SWHall;SWH];
+    DIRall = [DIRall;DIR];
+    
+    m1D = [m1D;[DIR(1:1416);DIR(8017:end)]];
+    m2D = [m2D;DIR(1417:3624)];
+    m3D = [m3D;DIR(3625:5832)];
+    m4D = [m4D;DIR(5833:8016)];
+    
+    m1S = [m1S;[SWH(1:1416);SWH(8017:end)]];
+    m2S = [m2S;SWH(1417:3624)];
+    m3S = [m3S;SWH(3625:5832)];
+    m4S = [m4S;SWH(5833:8016)];
+end
+
+season = ["Winter","Spring","Summer","Autumn","FullYear"];
+direc = {m1D,m2D,m3D,m4D,DIRall};
+swh = {m1S,m2S,m3S,m4S,SWHall};
+
+for i = 1:5
+    str = sprintf('SWAN-SWH\n%s',season(i));
+    figure('Color', 'w');
+    wind_rose(direc{i}, swh{i},'n',16,'quad',4,'lablegend','SWH(m)')
+    title(str, 'FontWeight', 'bold');
+    %saveas(gcf, sprintf('wind_rose_%s.png', season(i)));
+    %exportgraphics(gcf, sprintf('wind_rose_%s.eps', season(i)), 'ContentType', 'vector');
+    %exportgraphics(gcf, sprintf('wind_rose_%s.pdf', season(i)), 'ContentType', 'vector');
+    print(gcf, sprintf('wind_rose_%s.png', season(i)), '-dpng', '-r500'); % 500 DPI
+    close(gcf);
+end
+
--- RoseDiagram/wp_windRoseDraw.m
+++ RoseDiagram/wp_windRoseDraw.m
@@ -1,7 +1,7 @@
-function [] = windRoseDraw(SwanFileFolderPath)
-
-%% 2. 读取SWAN计算数据
-% BSG 9358 DSN 2027
+function [] = wp_windRoseDraw(SwanFileFolderPath)
+%读取SWAN计算数据
+%"E:\张明阳\项目\SWAN_Taiwan_result\te_ST6_S6_2022.mat"
+addpath('../CommonMethodology/');
 files = dir(SwanFileFolderPath);
 filePaths = {};
 
@@ -13,7 +13,8 @@ m1S = [];
 m2S = [];
 m3S = [];
 m4S = [];
-
+directionall = [];
+windall = [];
 
 for i = 1:length(files)
     % 检查是否是文件（排除 '.' 和 '..' 目录）
@@ -26,11 +27,15 @@ for i = 1:length(files)
 end
 
 for year = 1:length(filePaths)
-    windx = wp_readMat(filePaths{year},2027,2);%"E:\张明阳\项目\SWAN_Taiwan_result\te_ST6_S6_2022.mat"
+    % BSG 9358 DSN 2027
+    windx = wp_readMat(filePaths{year},2027,2);
     windy = wp_readMat(filePaths{year},2027,3);
 
     wind = sqrt(windx.*windx+windy.*windy);
     direction = atan2d(windx, windy);
+
+    windall = [windall;wind];
+    directionall = [directionall;direction];
     
     m1D = [m1D;[direction(1:1416);direction(8017:end)]];
     m2D = [m2D;direction(1417:3624)];
@@ -41,15 +46,14 @@ for year = 1:length(filePaths)
     m2S = [m2S;wind(1417:3624)];
     m3S = [m3S;wind(3625:5832)];
     m4S = [m4S;wind(5833:8016)];
-
 end
 
 season = ["Winter","Spring","Summer","Autumn","FullYear"];
-direc = {m1D,m2D,m3D,m4D,direction};
-spee = {m1S,m2S,m3S,m4S,wind};
+direc = {m1D,m2D,m3D,m4D,directionall};
+spee = {m1S,m2S,m3S,m4S,windall};
 
 for i = 1:5
-    str = sprintf('SWAN-DSN\n%s',season(i));
+    str = sprintf('SWAN-WIND\n%s',season(i));
     figure('Color', 'w');
     wind_rose(direc{i}, spee{i},'n',16,'quad',4,'lablegend','WindSpeed(m/s)')
     title(str, 'FontWeight', 'bold');
